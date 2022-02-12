924bbd8b060b50a90d82d28e7c3a9551772405bf
Paul Anigbo <paulanigbo@gmail.com>
2022-02-11 19:54:04 -0600

    mapped note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import Note from "../components/body/Note";
+import notes from "../notes";
 
 let App = () => {
   return (
     <div>
       <Header />
-      <Note
-        title="Just some random title"
-        note="Here is a note I made just from the bottom of my heart. I do really hope
-        you love it!"
-      />
+      {notes.map((note, index) => (
+        <Note key={index} title={note.title} note={note.content} />
+      ))}
       <Footer />
     </div>
   );
